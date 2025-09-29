d3da523d50909d3cbf7ae36ae88b75a9c1d9b743
Maciej Gad <gad.maciej@gmail.com>
2025-09-29 21:59:56 +0200

    feat: add limit parameter to get method in ClientProtocol and update request handling

--- Sources/ShoperWebAPI/Client.swift
+++ Sources/ShoperWebAPI/Client.swift
@@ -4,7 +4,7 @@ import FoundationNetworking
 #endif
 
 public protocol ClientProtocol {
-    func get(endpoint: Endpoint, id: Int?, filters: Filters?, sort: SortOrder?, page: Int?) async throws -> Data
+    func get(endpoint: Endpoint, id: Int?, filters: Filters?, sort: SortOrder?, page: Int?, limit: Int?) async throws -> Data
     func post(endpoint: Endpoint, payload: any Encodable) async throws -> Data
     func put(endpoint: Endpoint, id: Int, payload: any Encodable) async throws -> Data
     func delete(endpoint: Endpoint, id: Int) async throws -> Data
@@ -69,8 +69,8 @@ public final class Client {
 @available(macOS 12.0, *)
 extension Client: ClientProtocol {
     
-    public func get(endpoint: Endpoint, id: Int?, filters: Filters?, sort: SortOrder?, page: Int?) async throws -> Data {
-        try await request(endpoint, id: id, method: .get, filters: filters, sort: sort, page: page)
+    public func get(endpoint: Endpoint, id: Int?, filters: Filters?, sort: SortOrder?, page: Int?, limit: Int?) async throws -> Data {
+        try await request(endpoint, id: id, method: .get, filters: filters, sort: sort, page: page, limit: limit)
     }
     
     public func post(endpoint: Endpoint, payload: any Encodable) async throws -> Data {
@@ -89,7 +89,7 @@ extension Client: ClientProtocol {
         try decoder.decode(Model.self, from: data)
     }
     
-    private func request(_ endpoint: Endpoint, id: Int?, method: Method, payload: (any Encodable)? = nil, filters: Filters? = nil, sort: SortOrder? = nil, page: Int? = nil) async throws -> Data {
+    private func request(_ endpoint: Endpoint, id: Int?, method: Method, payload: (any Encodable)? = nil, filters: Filters? = nil, sort: SortOrder? = nil, page: Int? = nil, limit: Int? = nil) async throws -> Data {
         let filtersString: String?
         if let filters, !filters.isEmpty {
             let filtersData = try filterEncoder.encode(filters)
@@ -97,7 +97,7 @@ extension Client: ClientProtocol {
         } else {
             filtersString = nil
         }
-        let url = try endpoint.url(config: config, id: id, filters: filtersString, sort: sort, page: page)
+        let url = try endpoint.url(config: config, id: id, filters: filtersString, sort: sort, page: page, limit: limit)
         if config.verbose {
             print("Url: \(url)")
         }
