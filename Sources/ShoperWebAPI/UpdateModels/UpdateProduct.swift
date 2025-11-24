bc22d869e5692d98960168f591bc71c9edb647b3
Maciej Gad <gad.maciej@gmail.com>
2025-11-24 09:57:20 +0100

    add code to product update

--- Sources/ShoperWebAPI/UpdateModels/UpdateProduct.swift
+++ Sources/ShoperWebAPI/UpdateModels/UpdateProduct.swift
@@ -4,11 +4,20 @@ public struct UpdateProduct: Codable {
     /// stock update
     public let stock: UpdateStock?
     
+    /// code
+    public let code: String?
+    
     /// translations
     public let translations: [String: UpdateProductTranslation]?
     
-    public init(stock: UpdateStock? = nil, translations: [String : UpdateProductTranslation]? = nil) {
-        self.stock = stock
-        self.translations = translations
+
+    
+    public init(
+        stock: UpdateStock? = nil,
+        code: String? = nil,
+        translations: [String : UpdateProductTranslation]? = nil) {
+            self.stock = stock
+            self.code = code
+            self.translations = translations
     }
 }
